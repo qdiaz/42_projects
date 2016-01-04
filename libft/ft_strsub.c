3b8a94402db2af2b263cadf64fcd571bae3ff383
Quentin DIAZ <qdiaz@e1r4p18.42.fr>
2016-01-04 09:52:42 +0100

    my libft

---
+++ libft/ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qdiaz <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/30 15:54:46 by qdiaz             #+#    #+#             */
+/*   Updated: 2015/12/02 16:56:32 by qdiaz            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	i = 0;
+	str = ft_strnew(len);
+	if (str == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		str[i++] = s[start];
+		start++;
+	}
+	return (str);
+}
