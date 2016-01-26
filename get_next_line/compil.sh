72cf4cd58d8785977128dd79b99bcbd28fe762b8
Quentin DIAZ <qdiaz@e1r3p16.42.fr>
2016-01-26 12:50:43 +0100

    del useless files

--- get_next_line/compil.sh
+++
@@ -1,6 +0,0 @@
-make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
-clang  -Wall -Wextra -Werror -I libft/includes -o main.o -c main.c
-clang -o test_gnl main.o get_next_line.o -I libft/includes -L libft/ -lft
-rm -f get_next_line.o main.o
-echo "\033[32mtest_gnl was created\033[0m"
