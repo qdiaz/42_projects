2e28c7eb2a6420b89f72198921cc7e7a518c1338
Quentin DIAZ <qdiaz@e1r5p17.42.fr>
2016-01-28 16:15:08 +0100

    rm fichiers useless

--- ft_ls/compil.sh
+++
@@ -1,5 +0,0 @@
-make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -I libft/ -o ft_ls.o -c ft_ls.c
-clang -o ft_ls ft_ls.o -I libft/ -L libft/ -lft
-rm -f ft_ls.o
-echo "\033[32mft_ls was created\033[0m"
